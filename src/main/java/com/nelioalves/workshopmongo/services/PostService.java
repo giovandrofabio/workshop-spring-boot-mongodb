2553a329185a4b762829a71601c2737430a05e5c
Giovandro Santos <vsm.giovandro@gmail.com>
2024-02-20 16:23:07 -0300

    Obtendo um post por id

---
+++ src/main/java/com/nelioalves/workshopmongo/services/PostService.java
@@ -0,0 +1,32 @@
+package com.nelioalves.workshopmongo.services;
+
+import com.nelioalves.workshopmongo.domain.Post;
+import com.nelioalves.workshopmongo.repository.PostRepository;
+import com.nelioalves.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+//    public List<Post> findByTitle(String text) {
+//        return repo.searchTitle(text);
+//    }
+
+//    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+//        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+//        return repo.fullSearch(text, minDate, maxDate);
+//    }
+}
